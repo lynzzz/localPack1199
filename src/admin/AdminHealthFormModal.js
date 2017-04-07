30cc597ab9b202c463569de4db2d59d3aa7df125
Zeng <uwhfz3@net.plm.eds.com>
2017-04-07 12:53:23 -0500

    Style the modal box

--- src/admin/AdminHealthFormModal.js
+++ src/admin/AdminHealthFormModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactModal  from 'react-modal';
+import Modal  from 'react-modal';
 
 
 
@@ -23,51 +23,52 @@ class HealthFormModal extends React.Component{
 	closeModal(){
 		this.setState({isModalOpen : false})
 	}
-         
 
+    save(){
+	}
+
+
+
+
+    render(){
 	
-	
-    render(){	
-	
+	const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-30%, -30%)'
+  }
+};
+
+
 		return(
 			<div>
                 <button className="btn btn-warning" onClick={()=>this.openModal()}>Edit Participant</button>
-                <ReactModal 
-				     isOpen={this.state.isModalOpen} 
-					 contentLabel="Health Form Modal"
-                     style={{ overlay: {    
-	
-	position          : 'fixed',
-    top                        : '300px',
-    left                       : '300px',
-	width                      :  '25%',
-	height                     :  '50%'//,
-    //backgroundColor   : 'rgba(255, 255, 255, 0.75)'
-	}, 
-	
-	content: {
-	 
-	position                   : 'absolute',
-    
-    border                     : '1px solid #ccc',
-    background                 : '#fff',
-    overflow                   : 'auto',
-    WebkitOverflowScrolling    : 'touch',
-    borderRadius               : '4px',
-    outline                    : 'none',
-    padding                    : '20px'} }}
-				>
-				    <div className="dropdown">
-                        <button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Health Form
+                <Modal isOpen={this.state.isModalOpen} contentLabel="Health Form Modal" style = {customStyles}>
+               
+			        <div className="modal-header">
+					    Health Form option
+					</div>
+			   
+					 <div className="modal-header">
+					 <div  className="dropdown">						
+                        <button className="btn btn-primary dropdown-toggle" data-toggle="dropdown">Health Form
                         <span className="caret"></span></button>
                         <ul className="dropdown-menu">
                         <li><a href="#">Yes</a></li>
-                        <li><a href="#">NO</a></li>
+                        <li><a href="#">No</a></li>
                         </ul>
                     </div>
-					
-					<button onClick={()=>this.closeModal()}>close</button>
-				</ReactModal>
+					</div>
+
+					<div className="modal-footer">
+					    <button className="btn btn-primary" onClick={()=>this.save() }>save</button>
+					    <button className="btn btn-primary" onClick={()=>this.closeModal()}>cancel</button>
+					</div>
+				</Modal>
 			</div>
 		);
 	};
